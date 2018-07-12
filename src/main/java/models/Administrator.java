7fd0a33be8d2d2f4202b15c3fbb8addf52e1af15
Gary Muir <garymuir17@gmail.com>
2018-07-12 11:54:54 +0100

    sets up manager

--- src/main/java/models/Administrator.java
+++ src/main/java/models/Administrator.java
@@ -1,4 +1,28 @@
 package models;
 
-public class Administrator {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "administrators")
+public class Administrator extends Employee {
+
+    private Manager manager;
+
+    public Administrator() { }
+
+    public Administrator(String name, int nI, int salary, Manager manager) {
+        super(name, nI, salary);
+        this.manager = manager;
+    }
+
+    @Column(name = "manager")
+    public Manager getManager() {
+        return manager;
+    }
+
+    public void setManager(Manager manager) {
+        this.manager = manager;
+    }
 }
